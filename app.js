0f98c1322025ed414cfb60c7b1e3831a7d0c4ba5
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-18 21:57:53 +0530

    Seeding Campgrounds

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground')
 
-mongoose.connect('mongodb://localhost:27017/wild-campsy')
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console, "connection error:"));
@@ -18,6 +19,12 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=>{
     res.render('home')
+})
+
+app.get('/makecampground', async(req, res)=>{
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, ()=>{
