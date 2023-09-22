cfa22dc12c8a09653bf8ad5d1ecec5e31ae78ec4
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-22 10:44:57 +0530

    setting up flash, error and success

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const session = require('express-session')
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp'  );
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+   
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -29,18 +29,25 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
-const sessionConfig={
+
+const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        expires: Date.now() + 1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
 app.use(session(sessionConfig))
+app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
@@ -48,6 +55,7 @@ app.use('/campgrounds/:id/reviews', reviews)
 app.get('/', (req, res) => {
     res.render('home')
 });
+
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const router = express.Router({mergeParams: true});
-const catchAsync = require('../utils/catchAsync');
+const router = express.Router({ mergeParams: true });
+
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+
+const { reviewSchema } = require('../schemas.js');
+
+
 const ExpressError = require('../utils/ExpressError');
-const {  reviewSchema } = require('../schemas.js');
+const catchAsync = require('../utils/catchAsync');
 
 const validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
@@ -16,12 +20,15 @@ const validateReview = (req, res, next) => {
     }
 }
 
+
+
 router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Created new review!');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
@@ -29,6 +36,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review')
     res.redirect(`/campgrounds/${id}`);
 }))
 
