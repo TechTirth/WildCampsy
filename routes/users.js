3b96daf585c7bde2e287b035f91e4590aaf41572
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-23 11:24:02 +0530

    Showing and Hiding edit/delete

--- routes/users.js
+++ routes/users.js
@@ -41,10 +41,14 @@ router.post('/login',
         res.redirect(redirectUrl);
     });
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-})
+    router.get('/logout', (req, res, next) => {
+        req.logout(function (err) {
+            if (err) {
+                return next(err);
+            }
+            req.flash('success', 'Goodbye!');
+            res.redirect('/campgrounds');
+        });
+    });
 
 module.exports = router;
