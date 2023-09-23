3d1f886a6e8bd6085e037e6966822cbbe7b87118
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-23 11:00:34 +0530

    returnTo behavior

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
-   
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
