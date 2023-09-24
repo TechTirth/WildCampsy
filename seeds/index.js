8a32a47753b173162bb54acac0870bbe7fe2081f
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-24 23:19:44 +0530

    fixing our seeds

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,40 @@ const seedDB = async()=>{
             author: '650e5170e1b83a92d37656b6',
             location: `${cities[random150].city}, ${cities[random150].admin_name}`, 
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error dicta magnam, quos iusto tenetur necessitatibus eum quod, minus sapiente obcaecati aut deserunt nihil nobis, ullam veniam nostrum harum ratione dolore.',
-            price
+            price,
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574028/WildCampsy/igbp4bmz9ttl9owk6fzp.jpg',
+                  filename: 'WildCampsy/igbp4bmz9ttl9owk6fzp',
+                 
+                },
+                {
+                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574033/WildCampsy/vc5pi8rl1hxosybve690.jpg',
+                  filename: 'WildCampsy/vc5pi8rl1hxosybve690',
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574043/WildCampsy/ld7a4mbsfr5mfyjgswwv.jpg',
+                  filename: 'WildCampsy/ld7a4mbsfr5mfyjgswwv',
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574051/WildCampsy/untdhhzadsoqlkdhi0zk.jpg',
+                  filename: 'WildCampsy/untdhhzadsoqlkdhi0zk',
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574056/WildCampsy/s4satq7czfshhnm1trtx.jpg',
+                  filename: 'WildCampsy/s4satq7czfshhnm1trtx',
+                  
+                },
+                {
+                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574064/WildCampsy/bvttars2to280pzvtgws.jpg',
+                  filename: 'WildCampsy/bvttars2to280pzvtgws',
+                  
+                }
+              ]
 
         })
         await camp.save();
