85221faf84c6a22b9f0b8427469c6755af5eba7a
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-24 21:03:23 +0530

    Uloading to cloudinary basics

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ if(process.env.NODE_ENV != "prodeuction"){
     require('dotenv').config()
 }
 
-console.log(process.env.)
-console.log(process.env.)
+
 
 const express = require('express');
 const path = require('path');
