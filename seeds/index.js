6b7036a0c7f117eb9fdadff1c6169c28ff1b77f4
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-25 14:13:45 +0530

    reseeding our database again

--- seeds/index.js
+++ seeds/index.js
@@ -1,73 +1,54 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
-const Campground = require('../models/campground')
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const db = mongoose.connection;
-db.on("error",console.error.bind(console, "connection error:"));
-db.once("open", ()=>{
-    console.log("Database Connected")
-})
 
-const sample = array => array[Math.floor(Math.random()*array.length)]
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-const seedDB = async()=>{
+const seedDB = async () => {
     await Campground.deleteMany({});
-    const c = new Campground({title: 'purple field'});
-    for(let i=0;i<50;i++){
-        const random150 =  Math.floor(Math.random()*150);
-        const price = Math.floor(Math.random()*20)+10
+    for (let i = 0; i < 300; i++) {
+        const random150 = Math.floor(Math.random() * 150);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            //YOUR USER ID
             author: '650e5170e1b83a92d37656b6',
-            location: `${cities[random150].city}, ${cities[random150].admin_name}`, 
+            location: `${cities[random150].city}, ${cities[random150].admin_name}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error dicta magnam, quos iusto tenetur necessitatibus eum quod, minus sapiente obcaecati aut deserunt nihil nobis, ullam veniam nostrum harum ratione dolore.',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
-            geometry: { 
-              type: 'Point', 
-              coordinates: [ 72.579707, 23.021624 ] 
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random150].lng,
+                    cities[random150].lat,
+                ]
             },
             images: [
                 {
-                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574028/WildCampsy/igbp4bmz9ttl9owk6fzp.jpg',
-                  filename: 'WildCampsy/igbp4bmz9ttl9owk6fzp',
-                 
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
                 },
                 {
-                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574033/WildCampsy/vc5pi8rl1hxosybve690.jpg',
-                  filename: 'WildCampsy/vc5pi8rl1hxosybve690',
-                  
-                },
-                {
-                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574043/WildCampsy/ld7a4mbsfr5mfyjgswwv.jpg',
-                  filename: 'WildCampsy/ld7a4mbsfr5mfyjgswwv',
-                  
-                },
-                {
-                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574051/WildCampsy/untdhhzadsoqlkdhi0zk.jpg',
-                  filename: 'WildCampsy/untdhhzadsoqlkdhi0zk',
-                  
-                },
-                {
-                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574056/WildCampsy/s4satq7czfshhnm1trtx.jpg',
-                  filename: 'WildCampsy/s4satq7czfshhnm1trtx',
-                  
-                },
-                {
-                  url: 'https://res.cloudinary.com/dxbznqpea/image/upload/v1695574064/WildCampsy/bvttars2to280pzvtgws.jpg',
-                  filename: 'WildCampsy/bvttars2to280pzvtgws',
-                  
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
                 }
-              ]
-
+            ]
         })
         await camp.save();
     }
 }
 
-seedDB().then(()=>{
+seedDB().then(() => {
     mongoose.connection.close();
 })
