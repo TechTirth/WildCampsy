74956b2e4a30dbae5ee34d8fc07ae6545ff239ff
Tirth Vanparia <t1eerth.v@gmail.com>
2023-09-25 09:08:49 +0530

    deleting images backend

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,6 @@
-const { model } = require('mongoose');
 const Campground = require('../models/campground');
+const {cloudinary} = require("../cloudinary")
+
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -15,7 +16,6 @@ module.exports.createCampground = async (req, res, next) => {
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.author = req.user._id;
     await campground.save();
-    console.log(campground)
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 } 
@@ -52,6 +52,13 @@ module.exports.updateCampground = async (req, res) => {
     const imgs = req.files.map(f => ({url: f.path, filename: f.filename}))
     campground.images.push(...imgs)
     await campground.save()
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename)
+        }
+        await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}})
+        console.log(campground)
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }
